010d82119bd11a0109fdc496eb6fd02f47a5e15c
Fedecas <elfefecabjsape@gmail.com>
2019-01-27 03:54:08 -0300

    Transparent background in tab

--- src/main/kotlin/com/peinbol/client/PlayersInfoUI.kt
+++ src/main/kotlin/com/peinbol/client/PlayersInfoUI.kt
@@ -1,6 +1,8 @@
 package com.peinbol.client
 
+import org.lwjgl.nuklear.NkColor
 import org.lwjgl.nuklear.NkContext
+import org.lwjgl.nuklear.Nuklear
 import org.lwjgl.nuklear.Nuklear.*
 
 /** To shot latency, fps, etc. For devs. */
@@ -17,6 +19,7 @@ class PlayersInfoUI
 
     private val WIDTH_PER_CHARACTER = 8f
     private val longestMsg = players.maxBy { it.length }?.length ?: 1
+    private val BLACK_TRANSPARENT = Nuklear.nk_rgba(0, 0, 0, 100, NkColor.callocStack())
 
     private val windowWidth = longestMsg * WIDTH_PER_CHARACTER * 3
     private val windowHeight = 26f * (1 + players.size) // title + players
@@ -25,25 +28,24 @@ class PlayersInfoUI
 
     override fun draw(ctx: NkContext, screenWidth: Float, screenHeight: Float) {
         if (!visible) return
-        nkBeginDefaultWindow(
+        nkBeginTransparentWindow(
             ctx,
             "Players info",
                 (screenWidth / 2f) - (windowWidth / 2f),
                 (screenHeight / 2f) - (windowHeight / 2f),
-                windowWidth, windowHeight, NK_WINDOW_TITLE + NK_WINDOW_NO_SCROLLBAR
+                windowWidth, windowHeight,
+                NK_WINDOW_TITLE + NK_WINDOW_NO_SCROLLBAR,
+                BLACK_TRANSPARENT
         ) {
             for (player in players) {
                 nk_layout_row_dynamic(ctx, 20f, 3)
                 nk_label(ctx,
-                    //player.name, NK_TEXT_ALIGN_LEFT
                     player, NK_TEXT_ALIGN_LEFT
                 )
                 nk_label(ctx,
-                    //player.score.toString(), NK_TEXT_ALIGN_CENTERED
                     "55", NK_TEXT_ALIGN_CENTERED
                 )
                 nk_label(ctx,
-                    //player.score.toString(), NK_TEXT_ALIGN_RIGHT
                     "102 ms", NK_TEXT_ALIGN_RIGHT
                 )
             }
